64ab479b890873b93e4914532363b4ec6f5ef373
Thilo Borgmann <thilo@medal.tv>
2025-11-05 12:38:31 +0100

    avfilter: add eval functions

--- libavfilter/boxblur.c
+++ libavfilter/boxblur.c
@@ -22,6 +22,7 @@
 
 #include "libavutil/mem.h"
 #include "boxblur.h"
+#include "filters.h"
 
 static const char *const var_names[] = {
     "w",
@@ -30,6 +31,9 @@ static const char *const var_names[] = {
     "ch",
     "hsub",
     "vsub",
+    "n",
+    "pos",
+    "t",
     NULL
 };
 
@@ -40,22 +44,29 @@ enum var_name {
     VAR_CH,
     VAR_HSUB,
     VAR_VSUB,
+    VAR_N,
+    VAR_POS,
+    VAR_T,
     VARS_NB
 };
 
 
 int ff_boxblur_eval_filter_params(AVFilterLink *inlink,
+                                  AVFrame      *in,
                                   FilterParam *luma_param,
                                   FilterParam *chroma_param,
-                                  FilterParam *alpha_param)
+                                  FilterParam *alpha_param,
+                                  int         *hasTemporalExpressions)
 {
     const AVPixFmtDescriptor *desc = av_pix_fmt_desc_get(inlink->format);
     AVFilterContext *ctx = inlink->dst;
+    FilterLink* inl = ff_filter_link(inlink);
     int w = inlink->w, h = inlink->h;
     int cw, ch;
     double var_values[VARS_NB], res;
     char *expr;
     int ret;
+    int64_t pos;
 
     if (!luma_param->radius_expr) {
         av_log(ctx, AV_LOG_ERROR, "Luma radius expression is not set.\n");
@@ -86,8 +97,22 @@ int ff_boxblur_eval_filter_params(AVFilterLink *inlink,
     var_values[VAR_HSUB]    = 1<<(desc->log2_chroma_w);
     var_values[VAR_VSUB]    = 1<<(desc->log2_chroma_h);
 
+    if (in){
+        pos = in->pkt_pos;
+        var_values[VAR_N] = inl->frame_count_out;
+        var_values[VAR_T] = in->pts == AV_NOPTS_VALUE ? NAN : in->pts * av_q2d(inlink->time_base);
+        var_values[VAR_POS] = pos == -1 ? NAN : pos;
+    } else {
+        var_values[VAR_N] = inl->frame_count_out;
+        var_values[VAR_T] = 0;
+        var_values[VAR_POS] = NAN;
+    }
+
 #define EVAL_RADIUS_EXPR(comp)                                          \
     expr = comp->radius_expr;                                           \
+    if (!in && strchr(expr, 't')){                                      \
+        *hasTemporalExpressions = 1;                                    \
+    }                                                                   \
     ret = av_expr_parse_and_eval(&res, expr, var_names, var_values,     \
                                  NULL, NULL, NULL, NULL, NULL, 0, ctx); \
     comp->radius = res;                                                 \
